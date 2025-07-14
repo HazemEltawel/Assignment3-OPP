ffa22ee80f29f742ffa7bd2c6df34fb54c415ce9
HazemEltawel <eltawelhazem5@gmail.com>
2025-07-15 01:02:55 +0300

    Add project files.

---
+++ Assignment3-OPP/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_OPP
+{
+    using System;
+    using System.Globalization;
+
+   
+
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public Security security { get; set; }
+
+        private decimal salary;
+        public decimal Salary
+        {
+            get => salary;
+            set
+            {
+                if (value >= 0)
+                    salary = value;
+                else
+                    Console.WriteLine("Invalid salary amount!");
+            }
+        }
+
+        public DateTime HireDate { get; set; }
+
+        private char gender;
+        public char Gender
+        {
+            get => gender;
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    Console.WriteLine("Invalid gender. Only 'M' or 'F' allowed.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {id}\n" +
+                   $"Name: {Name}\n" +
+                   $"Gender: {(Gender == 'M' ? "Male" : "Female")}\n" +
+                   $"Security Level: {security}\n" +
+                   $"Salary: {Salary.ToString("C", CultureInfo.CurrentCulture)}\n" +
+                   $"Hire Date: {HireDate.ToShortDateString()}";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
---
+++ Experince3-OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Assignment3_OPP;
+class Program
+{
+    static void Main()
+    {
+
+        #region
+        //Design and implement a Class for the employees in a company:
+
+        Employee employee = new Employee()
+        {
+            id = 100,
+            Name = "Hazem",
+            security = Security.Developer,
+            Salary = 12000.50m,
+            HireDate = new DateTime(2023, 10, 1),
+        };
+        Console.WriteLine(employee.ToString());
+        #endregion
+    }
+}
